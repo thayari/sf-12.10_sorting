9999696227004f2d8074e22c24990ccca01503b4
thayari <alex.silvanovich@gmail.com>
2021-05-28 20:36:18 +0300

    minor change

--- js/index.js
+++ js/index.js
@@ -130,9 +130,7 @@ const sortAPI = {
     for (let i = 0; i < n - 1; i++) {
       for (let j = 0; j < n - 1 - i; j++) {
         if (comparation(arr[j], arr[j + 1])) {
-          let temp = arr[j + 1];
-          arr[j + 1] = arr[j];
-          arr[j] = temp;
+          [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
         }
       }
     }
